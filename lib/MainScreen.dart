ac3b3221b0269deb1d627147834f1aed997d7173
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-05-20 19:49:08 +0900

    commit

---
+++ lib/MainScreen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class MainScreenPage extends StatefulWidget {
+  const MainScreenPage({super.key});
+
+  @override
+  State<MainScreenPage> createState() => _MainScreenPageState();
+}
+
+class _MainScreenPageState extends State<MainScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
