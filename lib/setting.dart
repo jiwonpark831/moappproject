0288907e99a0ee17f0df4083fd9e779c27c02b1a
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-05-27 16:19:29 +0900

    commit

--- lib/setting.dart
+++ lib/setting.dart
@@ -10,6 +10,8 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("a"),);
+    return Scaffold(
+      body: Center(child: Text("a")),
+    );
   }
 }
