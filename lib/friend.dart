9bd6375f554d1bcd1ab93719c9e3d1c1fcf1f2ab
sudo-adduser-song <ssu00330@gmail.com>
2024-05-28 06:05:15 +0900

    friend, setting, timetable.dart commit

--- lib/friend.dart
+++ lib/friend.dart
@@ -10,6 +10,64 @@ class FriendPage extends StatefulWidget {
 class _FriendPageState extends State<FriendPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("This is friend page")));
+    return Scaffold(
+      appBar: AppBar(
+      ), 
+      body: SingleChildScrollView(child: Column(
+        children: [
+          Text('내 친구'),
+          Container(child: TextButton(
+            child:Text('친구 추가'),
+            onPressed:(){
+              debugPrint('친구 추가 버튼 클릭');
+            }
+          ),),
+          Divider(
+            indent: 25,
+            endIndent: 25,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              width: 300,
+              height: 200,
+              decoration: BoxDecoration(color: Color(0xffCCF9EE))),
+          SizedBox(
+            height: 30,
+          ),
+          Text('그룹'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              TextButton(
+                child:Text('그룹 추가'),
+                onPressed:(){
+                  debugPrint('그룹 추가 버튼 클릭');
+                }
+              ),
+              TextButton(
+                child:Text('그룹 생성'),
+                onPressed:(){
+                  debugPrint('그룹 생성 버튼 클릭');
+                }
+              ),
+            ]
+          ),
+          Divider(
+            indent: 25,
+            endIndent: 25,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+              width: 300,
+              height: 600,
+              decoration: BoxDecoration(color: Color(0xffCCF9EE))),
+          SizedBox(height: 30),
+        ]
+      ))
+    );
   }
 }
--- lib/timetable.dart
+++ lib/timetable.dart
@@ -46,67 +46,78 @@ class TimeTablePage extends StatefulWidget {
 class _TimeTablePageState extends State<TimeTablePage> {
   @override
   Widget build(BuildContext context) {
-    return TimeSchedulerTable(
-      cellHeight: 64,
-      cellWidth: 72,
-      columnTitles: const [
-        // You can assign any value to columnTitles. For Example : ['Column 1','Column 2','Column 3', ...]
-        "Mon",
-        "Tue",
-        "Wed",
-        "Thur",
-        "Fri",
-        "Sat",
-        "Sun"
-      ],
-      currentColumnTitleIndex: DateTime.now().weekday - 1,
-      rowTitles: const [
-        // You can assign any value to rowTitles. For Example : ['Row 1','Row 2','Row 3', ...]
-        '08:00 - 09:00',
-        '09:00 - 10:00',
-        '10:00 - 11:00',
-        '11:00 - 12:00',
-        '12:00 - 13:00',
-        '13:00 - 14:00',
-        '14:00 - 15:00',
-        '15:00 - 16:00',
-        '16:00 - 17:00',
-        '17:00 - 18:00',
-        '18:00 - 19:00',
-        '19:00 - 20:00',
-        '20:00 - 21:00',
-      ],
-      title: "Event Schedule",
-      titleStyle: const TextStyle(
-          fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-      eventTitleStyle: const TextStyle(color: Colors.white, fontSize: 8),
-      isBack: false, // back button
-      eventList: eventList,
-      eventAlert: EventAlert(
-        alertTextController: textController,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(12.0),
-        ),
-        addAlertTitle: "Add Event",
-        editAlertTitle: "Edit",
-        addButtonTitle: "ADD",
-        deleteButtonTitle: "DELETE",
-        updateButtonTitle: "UPDATE",
-        hintText: "Event Name",
-        textFieldEmptyValidateMessage: "Cannot be empty!",
-        addOnPressed: (event) {
-          // when an event added to your list
-          // Your code after event added.
-        },
-        updateOnPressed: (event) {
-          // when an event updated from your list
-          // Your code after event updated.
-        },
-        deleteOnPressed: (event) {
-          // when an event deleted from your list
-          // Your code after event deleted.
-        },
+    return Scaffold(
+      appBar: AppBar(
+      leading: IconButton(
+          icon: const Icon(Icons.cancel),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),    
+      title: const Text('TimeTable'),
       ),
+      body: TimeSchedulerTable(
+        cellHeight: 64,
+        cellWidth: 72,
+        columnTitles: const [
+          // You can assign any value to columnTitles. For Example : ['Column 1','Column 2','Column 3', ...]
+          "Mon",
+          "Tue",
+          "Wed",
+          "Thur",
+          "Fri",
+          "Sat",
+          "Sun"
+        ],
+        currentColumnTitleIndex: DateTime.now().weekday - 1,
+        rowTitles: const [
+          // You can assign any value to rowTitles. For Example : ['Row 1','Row 2','Row 3', ...]
+          '08:00 - 09:00',
+          '09:00 - 10:00',
+          '10:00 - 11:00',
+          '11:00 - 12:00',
+          '12:00 - 13:00',
+          '13:00 - 14:00',
+          '14:00 - 15:00',
+          '15:00 - 16:00',
+          '16:00 - 17:00',
+          '17:00 - 18:00',
+          '18:00 - 19:00',
+          '19:00 - 20:00',
+          '20:00 - 21:00',
+        ],
+        // title: "Event Schedule",
+        // titleStyle: const TextStyle(
+        //     fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+        // eventTitleStyle: const TextStyle(color: Colors.white, fontSize: 8),
+        isBack: false, // back button
+        eventList: eventList,
+        eventAlert: EventAlert(
+          alertTextController: textController,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(12.0),
+          ),
+          addAlertTitle: "Add Event",
+          editAlertTitle: "Edit",
+          addButtonTitle: "ADD",
+          deleteButtonTitle: "DELETE",
+          updateButtonTitle: "UPDATE",
+          hintText: "Event Name",
+          textFieldEmptyValidateMessage: "Cannot be empty!",
+          addOnPressed: (event) {
+            // when an event added to your list
+            // Your code after event added.
+          },
+          updateOnPressed: (event) {
+            // when an event updated from your list
+            // Your code after event updated.
+          },
+          deleteOnPressed: (event) {
+            // when an event deleted from your list
+            // Your code after event deleted.
+          },
+        ),
+      )
     );
   }
 }
